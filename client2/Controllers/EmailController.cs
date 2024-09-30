61218db68351170dda1a9d352d8d19e4da3bed6d
Van Ortega <ortegavan@live.com>
2024-09-30 10:29:56 -0300

    chore: ajustes nos textos do e-mail

--- client2/Controllers/EmailController.cs
+++ client2/Controllers/EmailController.cs
@@ -17,16 +17,21 @@ namespace EmailApi.Controllers
         }
 
         [HttpPost("send-test-email")]
-        public async Task<IActionResult> SendTestEmail([FromQuery] string email)
+        public async Task<IActionResult> SendTestEmail([FromQuery] string email, [FromQuery] string report)
         {
             if (string.IsNullOrEmpty(email))
             {
                 return BadRequest("Email address is required.");
             }
 
+            if (string.IsNullOrEmpty(report))
+            {
+                return BadRequest("Report name is required.");
+            }
+
             // Baixar o PDF da API Node/Express
             var client = _httpClientFactory.CreateClient();
-            var response = await client.GetAsync("http://localhost:3000/pdf?rep=report2");
+            var response = await client.GetAsync("http://localhost:3000/pdf?rep=" + report);
 
             if (!response.IsSuccessStatusCode)
             {
@@ -37,17 +42,17 @@ namespace EmailApi.Controllers
 
             // Criar o e-mail
             var message = new MimeMessage();
-            message.From.Add(new MailboxAddress("", ""));
+            message.From.Add(new MailboxAddress("Vanessa Ortega", "vanessaort@gmail.com"));
             message.To.Add(new MailboxAddress("", email));
-            message.Subject = "Hello World";
+            message.Subject = "Report";
 
             var bodyBuilder = new BodyBuilder
             {
-                TextBody = "Hello World"
+                TextBody = "Your report is attached to this email."
             };
 
             // Anexar o PDF
-            bodyBuilder.Attachments.Add("report2.pdf", pdfBytes, new ContentType("application", "pdf"));
+            bodyBuilder.Attachments.Add("report.pdf", pdfBytes, new ContentType("application", "pdf"));
             message.Body = bodyBuilder.ToMessageBody();
 
             // Configurar o envio de e-mail
